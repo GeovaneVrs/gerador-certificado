3e3380e39f92aa3558f47fb8bfed8f48a2ecc045
RM-1807 <geovanevalerio1807@gmail.com>
2025-06-03 23:57:48 -0300

    Componente: Secondary Button

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { Button } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Button],
+  imports: [Navbar, Button, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
