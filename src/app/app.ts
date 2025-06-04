ade7013be6dd749f82e02c742fe82acb944d8310
RM-1807 <geovanevalerio1807@gmail.com>
2025-06-03 23:50:18 -0300

    Componente: Primary Button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
+import { Button } from "./_components/primary-button/primary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Button],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
